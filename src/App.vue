e762b087a0c06e8f1dd82db213364f38792af46c
chenqiangsheng <466712071@qq.com>
2016-10-22 22:56:04 +0800

    src

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template  >
+  <div id="app" >
+    <top-bar></top-bar>
+    <top-header></top-header>
+    <top-banner></top-banner>
+    <top-sub></top-sub>
+    <goods></goods>
+    <temp-footer></temp-footer>
+    <play-video :play-config="playConfig"></play-video>
+    <!-- 这里动态的传递给子子组件 通过:props  -->
+  </div>
+</template>
+
+<script>
+import bus from './components/common/store'
+import TopBar from './components/TopBar'
+import TopHeader from './components/TopHeader'
+import TopBanner from './components/TopBanner'
+import TopSub from './components/TopSub'
+import Goods from './components/Goods'
+import TempFooter from './components/TempFooter'
+import PlayVideo from './components/common/PlayVideo'
+  console.log(bus)
+  export default {
+    data () {
+      return {
+        playConfig: {}
+      }
+    },
+    mounted () {
+     this.recivePlayVideo()
+    },
+    methods: {
+      recivePlayVideo (opts) {
+        bus.$on('play', (opts) => {
+          this.playConfig = opts
+        })
+      }
+    },
+    components: {
+      'top-bar': TopBar,
+      'top-header': TopHeader,
+      'top-banner': TopBanner,
+      'top-sub': TopSub,
+      'goods': Goods,
+      'temp-footer': TempFooter,
+      'play-video': PlayVideo
+    }
+  }
+
+</script>
+<style>
+  @import './assets/css/base.css';
+</style>
---
+++ src/components/common/PlayVideo.vue
@@ -0,0 +1,122 @@
+<template>
+	<div class="video-mask" v-show="maskStatus">
+		<transition name="slideIn"   >
+			<div class="video-content" v-show="videoStatus" >
+				<div class="video-header clearfix">
+					<h3 class="title-txt">{{playConfig.title}}</h3>
+					<div class="cancel-link"
+						@click="evtCancel">
+						<i class="cancel-btn fa fa-times"></i>
+					</div>
+				</div>
+				<div class="video">
+					<iframe width="880" height="536" :src="playConfig.videoUrl" frameborder="0" allowfullscreen=""></iframe>
+				</div>
+		</div>
+		</transition>
+	</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      maskStatus: false
+    }
+  },
+  computed: {
+    videoStatus () {
+      return this.playConfig.status
+    }
+  },
+  watch: {
+    videoStatus (newVal, oldVal) {
+      const that = this
+      if (!newVal) {
+        setTimeout(function () {
+          that.maskStatus = that.videoStatus
+        }, 500)
+      } else {
+        that.maskStatus = that.videoStatus
+      }
+    }
+  },
+	methods: {
+		evtCancel () {
+			this.playConfig.status = false
+			this.playConfig.videoUrl = ''
+		}
+	},
+	props: {
+		playConfig: {
+			required: true,
+			type: Object
+		}
+	}
+}
+</script>
+
+<style scoped>
+.video-mask {
+	position: fixed;
+	left: 0;
+	top: 0;
+	width: 100%;
+	height: 100%;
+	background: rgba(0, 0, 0, 0.5);
+	z-index: 12;
+}
+
+.video-content {
+	position: absolute;
+	left: 50%;
+	margin-left: -440px;
+	width: 880px;
+	height: 596px;
+	background: #fff;
+}
+
+.video-header {
+	padding: 14px 20px;
+	background: #f5f5f5;
+	.title-txt {
+		margin: 0;
+		font-size: 18px;
+		font-weight: 400;
+		line-height: 32px;
+		color: #424242;
+	}
+	.cancel-link {
+		display: block;
+		position: absolute;
+		top: 10px;
+		right: 20px;
+		width: 30px;
+		height: 30px;
+		line-height: 30px;
+		text-align: center;
+		border-radius: 30px;
+		font-size: 24px;
+		color: #757575;
+		transition: all .2s;
+		cursor: pointer;
+		&:hover {
+			color: #fff;
+			background: #e53935;
+		}
+	}
+}
+
+.video {
+	width: 880px;
+	height: 536px;
+}
+
+.slideIn-enter-active,.slideIn-leave-active{
+	transition: all .3s ease-out;
+	transform: translateY(30%);
+}
+.slideIn-enter,.slideIn-leave-active{
+	transform: translateY(-100%);
+}
+</style>
